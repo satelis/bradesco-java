cff4982ec59e46403f37bbe4d1a7895596cd54c0
Henrique Satelis <henriquefsatelis@gmail.com>
2025-05-01 11:47:44 -0300

    feat first set exercise

---
+++ dio-programacao-orientada-a-objetos/java-collections/set/operacoces-basicas-set/src/ConjuntoConvidados.java
@@ -0,0 +1,52 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados() {
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite) {
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidadoPorCodigo(int codigoConvite) {
+        Convidado convidadoRemovido = null;
+
+        for (Convidado c : convidadoSet) {
+            if (c.getCodigoConvite() == codigoConvite) {
+                convidadoRemovido = c;
+                break;
+            }
+        }
+        convidadoSet.remove(convidadoRemovido);
+    }
+
+    public int contarConvidados() {
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados() {
+        System.out.println(convidadoSet);
+    }
+
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " dentro do set de Convidados.");
+
+        conjuntoConvidados.adicionarConvidado("Convidado 1", 1234);
+        conjuntoConvidados.adicionarConvidado("Convidado 2", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 3", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 4", 1236);
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " dentro do set de Convidados.");
+
+        conjuntoConvidados.removerConvidadoPorCodigo(1234);
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " dentro do set de Convidados.");
+
+        conjuntoConvidados.exibirConvidados();
+    }
+}
